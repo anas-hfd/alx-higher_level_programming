05fdebe392c9045376488b03601fc94ea9835478
anas-hfd <anas1.hfd@gmail.com>
2023-06-05 10:24:35 -0700

    nothing

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the list contains a cycle
+ * @list: the list
+ * Return: cycles
+ */
+
+int check_cycle(listint_t *list)
+{
+}
