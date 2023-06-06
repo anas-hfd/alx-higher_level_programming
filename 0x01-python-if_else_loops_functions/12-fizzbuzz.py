325b2634636fa72e7120c8c68d42fbd7f5b25c37
anas-hfd <anas1.hfd@gmail.com>
2023-06-06 11:57:47 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
             print(f"FizzBuzz", end=" ")
-        if x % 3 == 0:
+        elif x % 3 == 0:
             print(f"Fizz", end=" ")
-        if x % 5 == 0:
+        elif x % 5 == 0:
             print(f"Buzz", end=" ")
         else:
             print(f"{x}", end=" ")
