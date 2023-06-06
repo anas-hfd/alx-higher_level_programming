63a6778720fed481acb68d3b1f1081a48be92fb1
anas-hfd <anas1.hfd@gmail.com>
2023-06-06 15:51:46 -0700

    rev  alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for alpha in range(ord('z'), ord('A') - 1, -1):
-    if alpha % 2 == 0:
-        print(chr(alpha).lower(), end="")
-    else:
-        print(chr(alpha).upper(), end="")
+for alphabet in range(122, 96, -1):
+    if alphabet % 2 != 0:
+        alphabet = alphabet - 32
+    print("{:s}".format(chr(alphabet)), end='')
