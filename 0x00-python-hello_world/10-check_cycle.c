dc4d4254a42da587d932202df47386119ad3077b
anas-hfd <anas1.hfd@gmail.com>
2023-06-06 05:20:28 -0700

    trying to check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,4 +9,24 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *check, *curr;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+	curr = list;
+	check = curr->next;
+
+	while (curr != NULL && check->next != NULL
+		&& check->next->next != NULL)
+	{
+		if (curr == check)
+		{
+			return (1);
+		}
+		curr = curr->next;
+		check = check->next->next;
+	}
+	return (0);
 }
