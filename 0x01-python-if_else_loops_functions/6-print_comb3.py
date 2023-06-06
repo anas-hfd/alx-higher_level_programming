fc9690cb34e5115840bb6fec30069ac6ad4da609
anas-hfd <anas1.hfd@gmail.com>
2023-06-06 10:58:22 -0700

    possible combs

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for tens in range(10):
     for ones in range(tens + 1, 10):
-        print("{:02d}".format(tens * 10 + ones), end=", "
-                if tens != 8 or ones != 9 else "\n")
+        number = tens * 10 + ones
+        print("{:02d}".format(number), end=", " if number < 89 else "\n")
