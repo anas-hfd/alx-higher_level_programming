253aaf5dc2a39a868617c2722c8e2b6033d2c70a
anas-hfd <anas1.hfd@gmail.com>
2023-06-09 12:41:21 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif length > 1:
         print("{:d} argument{:s}".format(length-1, "s:" if length > 2 else ":"))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, argv[i]))
+        for i in range(1, length):
+            print("{:d}: {:s}".format(i, argv[i]))
