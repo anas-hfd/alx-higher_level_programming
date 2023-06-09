64b1fb4fa67e0138c1846b93524adc379b164daf
anas-hfd <anas1.hfd@gmail.com>
2023-06-09 12:48:35 -0700

    another try

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv)
     if length == 1:
-        print("0 arguments.")
+        print("0 arguments.".format())
     elif length > 1:
         print("{:d} argument{:s}".format(length-1, "s:" if length > 2 else ":"))
         for i in range(1, length):
