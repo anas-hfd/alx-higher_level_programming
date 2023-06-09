b95ed253d1bda36e43e40b1ea043ec2276e40e6e
anas-hfd <anas1.hfd@gmail.com>
2023-06-09 12:35:10 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+length = len(argv)
+if length == 1:
+    print("0 arguments.")
+elif length > 1:
+    print("{:d} argument{:s}".format(length-1, "s:" if length > 2 else ":"))
+for i in range(1, length):
+    print("{:d}: {:s}".format(i, argv[i]))
