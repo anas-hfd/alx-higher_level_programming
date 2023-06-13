e73e694a34980d23498d6017c13503be3f3b3698
anas-hfd <anas1.hfd@gmail.com>
2023-06-13 15:26:33 -0700

    cpy

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+#include <listobject.h>
+
+/**
+ * print_python_list_info - Prints a Python list
+ * @p: a python object
+ * Return: void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t s, i, alloc;
+	PyObject *object;
+
+	s = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", s);
+	printf("[*] Allocated = %ld\n", alloc);
+	for (i = 0; i < s; i++)
+	{
+		object = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
+	}
+}
