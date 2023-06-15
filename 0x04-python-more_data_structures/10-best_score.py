624117bdc3f8aea8fb577f8b96c8dd88d5fb64ce
anas-hfd <anas1.hfd@gmail.com>
2023-06-15 01:10:27 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     best = max(a_dictionary, key=a_dictionary.get)
     return best
