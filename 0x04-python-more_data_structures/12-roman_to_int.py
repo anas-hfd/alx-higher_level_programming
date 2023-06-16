9f12b9a21766f7fcd12e191875e5b4045c2e4511
anas-hfd <anas1.hfd@gmail.com>
2023-06-16 04:53:15 -0700

    while in rome, do as the romans do

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,19 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    roman_nu = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_nu = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     dects = [roman_nu[x] for x in roman_string]
-    output = 0
+    out = 0
     for i in range(len(dects)):
-        output += dects[i]
+        out += dects[i]
         if dects[i - 1] < dects[i] and i != 0:
-            output -= (dects[i - 1] + dects[i - 1])
-    return output
+            out -= (dects[i - 1] + dects[i - 1])
+    return out
