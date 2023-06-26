2ebbcff76e30a3c853244c809f4fb4e712515bdb
anas-hfd <anas1.hfd@gmail.com>
2023-06-26 08:53:54 -0700

    just ints

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,14 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
 
-    try:
-        for n in my_list:
-            if count == x:
-                break
-            if isinstance(n, int):
-                print("{:d}".format(n), end=" ")
-                count += 1
-    except TypeError:
-        pass
+    for n in range(x):
+        try:
+            print("{:d}".format(my_list[n]), end=" ")
+            count += 1
+        except (TypeError, ValueError):
+            pass
     print("")
     return count
