c85a25f3eb5d9c46a24549c134d3ffd6ce65ea21
anas-hfd <anas1.hfd@gmail.com>
2023-06-26 08:55:13 -0700

    just ints

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for n in range(x):
         try:
-            print("{:d}".format(my_list[n]), end=" ")
+            print("{:d}".format(my_list[n]), end="")
             count += 1
         except (TypeError, ValueError):
-            pass
+            continue
     print("")
     return count
