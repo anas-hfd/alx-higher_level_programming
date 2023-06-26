cbe0fceffee0e16e69d7b8820e7d92b6da6893a2
anas-hfd <anas1.hfd@gmail.com>
2023-06-26 04:03:05 -0700

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for n in range(x):
         try:
-            print (my_list[i], end="")
+            print(my_list[n], end="")
             count += 1
         except IndexError:
             pass
-        print ("")
+    print()
     return count
