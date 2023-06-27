b548deec7cdd4b8f7ad90860d024d7cfb9956f57
anas-hfd <anas1.hfd@gmail.com>
2023-06-27 06:51:31 -0700

    size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square class definition """
+
+
+class Square:
+    """ attributes """
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
