8eb8c34fc5d16118de9aee57cfd7e17ab32b8291
anas-hfd <anas1.hfd@gmail.com>
2023-06-27 17:13:19 -0700

    singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" SLL class Node def """
+
+
+class Node:
+    """ attributes """
+    def __init__(self, data, next_node=None):
+        """data init"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Get data"""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        """Set data"""
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Get next node"""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set next node"""
+        if (value is not None and not isinstance(value, Node)):
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """ class: singly linked list"""
+
+    def __init__(self):
+        """data init"""
+        self.head = None
+
+    def __str__(self):
+        """Print str"""
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
+
+    def sorted_insert(self, value):
+        """ Inserts a node into a sorted linked list. """
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
