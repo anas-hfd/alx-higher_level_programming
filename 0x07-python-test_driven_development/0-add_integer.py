1991216b4c536edcbac97718cf8f3ee8aeac7f1c
anas-hfd <anas1.hfd@gmail.com>
2023-07-04 09:43:55 -0700

    add int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" add_integer()"""
+
+
+def add_integer(a, b=98):
+    """add_integer returns the sum of two integers"""
+    if type(a) not in [int, float] or a is None:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float] or b is None:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
