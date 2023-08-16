4b5c0e8d248ae340a7b0f607d4b9a695ad4a8871
anas-hfd <anas1.hfd@gmail.com>
2023-08-16 10:50:50 -0700

    previlige

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- a script that lists all privileges of the users user_0d_1 and user_0d_2.
+-- shows  privileges of the user_0d_1 and user_0d_2.
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
