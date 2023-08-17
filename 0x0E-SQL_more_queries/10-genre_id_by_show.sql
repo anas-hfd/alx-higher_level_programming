eb8ae1aab149dc0b3c0a458a74f7aaefba03aaa3
anas-hfd <anas1.hfd@gmail.com>
2023-08-17 09:58:41 -0700

    genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,15 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    hbtn_0d_tvshows.tv_shows
+JOIN
+    hbtn_0d_tvshows.tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.tv_show_id
+WHERE
+    tv_show_genres.genre_id IS NOT NULL
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
