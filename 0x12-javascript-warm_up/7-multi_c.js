ffd951a0d62914ea82537910ab3a7ae243792ab3
anas-hfd <anas1.hfd@gmail.com>
2023-09-12 08:16:37 -0700

    js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i;
+  for (i = 0; i < x; i++) {
+  console.log('C is fun');
+  }
+}
