ebd7fc4d6893a5ac7701144fa6946ca598215744
anas-hfd <anas1.hfd@gmail.com>
2023-09-28 10:18:18 -0700

    test

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept
-curl -s -I "$1" | grep "Allow" | awk '{print $2}'
+curl -s -I "$1" | grep "Allow" | cut -d " " -f 2-
