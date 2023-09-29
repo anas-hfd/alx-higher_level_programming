d5f7842cdeec3a2241325b9a490c52949ebbf963
anas-hfd <anas1.hfd@gmail.com>
2023-09-29 10:43:24 -0700

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,8 +10,10 @@ import urllib.request
 
 if __name__ == '__main__':
     url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(request) as response:
             body = response.read().decode("utf-8")
             print(body)
     except urllib.error.HTTPError as err:
